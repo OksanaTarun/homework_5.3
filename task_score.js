5579d89bf295f8bd36f0dec5ae2b453ab769d8de
OksanaTarun <oxana.tarun@gmail.com>
2022-10-25 23:29:16 +0300

    initial commit

---
+++ task_score.js
@@ -0,0 +1,6 @@
+let introduction = 100;
+let git = 319;
+let js = 120;
+let avgAll = (introduction + git + js)/3;
+let fullName = 'Oksana Tarun';
+console.log(`${fullName}. Average score for all modules: ${avgAll}.`)
